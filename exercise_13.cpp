00fb70fbd74d10ab202ee7f44939f5cb9954c8fd
korven48 <korven48@gmail.com>
2020-10-08 11:42:28 +0200

    Second class

--- exercise_13.cpp
+++ exercise_13.cpp
@@ -1,10 +1,24 @@
 
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    /* code */
+    const int SIZE = 3;
+
+    cout << "Introduce tres numeros separados por espacios:";
+    int arr[SIZE];
+    for (int i=0; i<SIZE; i++){
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + SIZE);
+
+    for (int i = 0; i < SIZE; i++){
+        cout << arr[i] << endl;
+    }
+
     return 0;
 }
--- exercise_16.cpp
+++ exercise_16.cpp
@@ -5,6 +5,16 @@ using namespace std;
 
 int main()
 {
-    /* code */
+    int inp;
+    do{
+        cin >> inp;
+        if (inp > 0){
+            if (inp % 2 == 0){
+                cout << "Its even" << endl;
+            } else { 
+                cout << "Its odd" << endl;
+                }
+        }
+    } while (inp != 0);
     return 0;
 }
