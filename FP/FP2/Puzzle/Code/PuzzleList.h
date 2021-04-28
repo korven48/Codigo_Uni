040e392fc70d8dec822a09dcef99673aaf3a8682
korven48 <korven48@gmail.com>
2021-04-28 21:02:04 +0200

    a

--- FP/FP2/Puzzle/Code/PuzzleList.h
+++ FP/FP2/Puzzle/Code/PuzzleList.h
@@ -1,1 +1,27 @@
-#pragma once
+#ifndef PUZZLELIST_H
+#define PUZZLELIST_H
+#include "Puzzle.h"
+
+const int MAX_PUZZLES = 100;
+
+typedef tPuzzlePtr *Puzzle;
+
+struct tPuzzlesList {
+	// This list is always kept ordered in increasing order of the maximum number  of  attempts  to  complete  the  puzzle
+	int count; 
+	tPuzzlePtr[MAX_PUZZLES] data;
+}
+
+const int nDimensions = 2;
+
+typedef tPuzzlesList tGatheredPuzzles[nDimensions];
+
+
+void initialize(t GatheredPuzzles& gp);
+bool load(t GathertedPuzzles& gp);
+void save(const tGatheredPuzzles& gp);
+int choosePuzzle(const PuzzlesList& pl);
+bool sortedInsert(const PuzzlesList& pl, tPuzzle* p);
+bool find(const tPuzzlesList& pl, tPuzzle* p, int& pos);
+
+#endif
